93012bbd1ee29f50243cf7c8393249ec1e41b9e7
Max <steblevskiymaxim@ua.fm>
2022-11-08 13:34:58 +0200

    added package lectures1and2

---
+++ lectures1and2/Main.java
@@ -0,0 +1,9 @@
+package lectures1and2;
+
+/**
+ * @author Max Steblevskiy(JavaAbuser)
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ lectures1and2/task2/SecondSolution.java
@@ -0,0 +1,41 @@
+package lectures1and2.task2;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.stream.Collectors;
+
+public class SecondSolution {
+    public Map<String, Integer> getTopFiveHashTags(List<String> list) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        for (String line : list) {
+            boolean wasMentioned = false;
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                if (word.startsWith("#")) {
+                    if (!resultMap.containsKey(word)) {
+                        resultMap.put(word, 1);
+                        wasMentioned = true;
+                    } else {
+                        if (!wasMentioned) {
+                            resultMap.put(word, resultMap.get(word) + 1);
+                        }
+                    }
+                }
+            }
+        }
+        resultMap = resultMap.entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .limit(5)
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldVal, newVal) -> oldVal,
+                        LinkedHashMap::new));
+
+        return resultMap;
+    }
+}

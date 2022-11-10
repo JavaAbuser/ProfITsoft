de30621e250012146fb4f14c508890d9fa6b0528
Max <steblevskiymaxim@ua.fm>
2022-11-10 15:41:00 +0200

    changed imports

--- lectures1and2/main/java/task2/SecondSolution.java
+++ lectures1and2/main/java/task2/SecondSolution.java
@@ -1,7 +1,11 @@
 package task2;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.LinkedHashMap;
 
 public class SecondSolution {
     public Map<String, Integer> getTopFiveHashTags(List<String> list) {

9604ea62d42a75f9569e2e5e5c6c99654d52cd74
Max <steblevskiymaxim@ua.fm>
2022-11-22 02:29:29 +0200

    initialization commit

---
+++ lectures3and4/main/java/task3/Main.java
@@ -0,0 +1,10 @@
+package task3;
+
+/**
+ * @author Max Steblevskiy(JavaAbuser)
+ */
+public class Main {
+    public static void main(String[] args){
+        XmlParser.parseXml("input.xml");
+    }
+}

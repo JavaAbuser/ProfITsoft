bc4826ca00541c721b4eb9f0542213a82d470a3f
Max <steblevskiymaxim@ua.fm>
2022-12-05 11:04:16 +0200

    added Main

---
+++ lectures3and4/main/java/task4/Main.java
@@ -0,0 +1,8 @@
+package task4;
+
+public class Main {
+    public static void main(String[] args) {
+        ViolationParser parser = new ViolationParser();
+        parser.parse("TrafficViolations");
+    }
+}

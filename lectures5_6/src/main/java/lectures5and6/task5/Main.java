eca2476d8aec4a30611f31052adaf42a4fbbd845
Max <steblevskiymaxim@ua.fm>
2022-12-09 17:45:08 +0200

    deleted directory

--- lectures5_6/src/main/java/lectures5and6/task5/Main.java
+++
@@ -1,8 +0,0 @@
-package lectures5and6.task5;
-
-public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        ViolationParser parser = new ViolationParser();
-        parser.parse("TrafficViolations");
-    }
-}

614a64e3dba6b3288cd7355d1db877ad9c333531
Max <steblevskiymaxim@ua.fm>
2022-12-10 04:34:23 +0200

    added annotations directory

---
+++ lectures5and6/src/main/java/lectures5and6/task6/annotations/Property.java
@@ -0,0 +1,13 @@
+package lectures5and6.task6.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Property {
+    String value() default "";
+    String format() default "";
+}

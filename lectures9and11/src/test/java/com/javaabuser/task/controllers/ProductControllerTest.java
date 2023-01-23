6d99aae4821fec05f4582f3060001a8d84a08732
Max <steblevskiymaxim@ua.fm>
2023-01-23 14:55:54 +0200

    updated post method testing

--- lectures9and11/src/test/java/com/javaabuser/task/controllers/ProductControllerTest.java
+++ lectures9and11/src/test/java/com/javaabuser/task/controllers/ProductControllerTest.java
@@ -11,27 +11,33 @@ import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockReset;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.aspectj.apache.bcel.classfile.Utility.codeToString;
 import static org.aspectj.apache.bcel.classfile.Utility.format;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.RequestEntity.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -44,17 +50,14 @@ class ProductControllerTest {
     private String page;
     private String perPage;
 
-    private void setUpValues(String name, String description, String page, String perPage){
-        this.name = name;
-        this.description = description;
-        this.page = page;
-        this.perPage = perPage;
-    }
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
     private ProductController productController;
+
+    @MockBean
+    private RestTemplate restTemplate;
 
     private CategoryDTO category1;
     private CategoryDTO category2;
@@ -66,6 +69,14 @@ class ProductControllerTest {
         category2 = new CategoryDTO("toy");
         category5 = new CategoryDTO("sport");
     }
+
+    private void setUpValues(String name, String description, String page, String perPage){
+        this.name = name;
+        this.description = description;
+        this.page = page;
+        this.perPage = perPage;
+    }
+
     @Test
     void getProducts() throws Exception {
         setUpValues(null, null, null, null);
@@ -141,33 +152,24 @@ class ProductControllerTest {
 }
 
     @Test
-    void createProduct() {
+    void createProduct() throws Exception {
         String productName = "city2";
-        String description = "constructor lego";
+        String productDescription = "constructor lego";
 
         String body = """
        {
            "name": "%s",
            "description": "%s",
            "category": "%s"
-       }""".formatted(productName, description, category2);
+       }""".formatted(productName, productDescription, category2);
+
+        ProductDTO product = new ProductDTO(productName, productDescription, category2);
 
         mockMvc.perform(post("/products")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(body)
                 )
-                .andExpect(status().isCreated())
-                .andReturn();
-
-
-        when(productController.createProduct(product)).thenReturn(
-                new ProductDTO("bmw X5 model", "1to21 size model of car", category2)
-        );
-
-        mockMvc.perform(get("/products/3")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(print());
+                .andExpect(status().isOk());
     }
 
     @Test
